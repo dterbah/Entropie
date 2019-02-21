ff012f270c29ec23401fdbe1a8d0c8b2ab5713f5
Terbah <dorian.terb@gmail.com>
2019-02-21 19:55:44 +0100

    Redirection

--- assets/js/dynamics/redirect.js
+++ assets/js/dynamics/redirect.js
@@ -1,9 +1,16 @@
+const SUBJECT_SEPARATOR_FOR_REDIRECTION = "|";
+const RESULT_URL = "result.php";
+
 $(document).ready(function() {
     $('.' + studentClass).on('click', function(event) {
         var activeSubjects = getActiveSubjects();
         if(activeSubjects.length == 0) {
             $('.w3-modal').css('display', 'block');
         }
+
+        const student = $(this).text();
+
+        redirect(RESULT_URL, student, activeSubjects);
     }); 
 }); 
 
@@ -13,6 +20,25 @@ $(document).ready(function() {
     });
 });
 
-function redirect(url) {
+function redirect(url, student, subjects) {
+    //params for the url
+    var urlParams = {};
+    urlParams['student'] = student;    
 
+    var urlSubjects = "";
+
+    subjects.forEach( (subject, index) => {
+        urlSubjects += subject + SUBJECT_SEPARATOR_FOR_REDIRECTION;
+    });
+
+    urlParams['subjects'] = urlSubjects;
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: "student=" + urlParams['student'] + "&subjects=" + urlParams['subjects'],
+        success: function() {
+            alert('ok');
+        }
+    });
 }
